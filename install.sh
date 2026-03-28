f9add1239cf7341988292a87925d963e0db90178
akko888 <axuval123@gmail.com>
2026-03-28 15:20:42 -0600

    Install and Uninstall scripts So, I dicided to make scripts to install and run this script like any other command. I decided to call it kirby temporally. Also there some improvements in the .gitignore file

---
+++ install.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+set -e
+
+PROJECT_NAME="kirby"
+INSTALL_LIB_DIR="/usr/local/lib/$PROJECT_NAME"
+INSTALL_BIN_LINK="/usr/local/bin/$PROJECT_NAME"
+
+echo "[INFO] Installing $PROJECT_NAME ..."
+
+if [ ! -d "bin" ] || [ ! -d "lib" ]; then
+    echo "[ERROR] You must run this script from project's root" 
+    exit 1
+fi
+
+if [ -d "$INSTALL_LIB_DIR" ]; then
+    echo "[WARNING] Existing installation found. Overwriting..."
+fi
+
+echo "[INFO] CREATING DIRECTORY AT $INSTALL_LIB_DIR"
+sudo mkdir -p "$INSTALL_LIB_DIR"
+
+echo "[INFO] COPYING FILES..."
+sudo cp -r bin "$INSTALL_LIB_DIR"
+sudo cp -r lib "$INSTALL_LIB_DIR"
+
+sudo chmod +x "$INSTALL_LIB_DIR/bin/main.sh"
+
+echo "[INFO] CREATING COMMAND: $INSTALL_BIN_LINK"
+sudo ln -sf "$INSTALL_LIB_DIR/bin/main.sh" "$INSTALL_BIN_LINK"
+
+echo "[SUCCESS] INSTALLED SUCCESSFULLY!"
+echo "You can run now"
+echo "$PROJECT_NAME -n MyProject -r /path -l java" 
---
+++ uninstall.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+set -e
+
+PROJECT_NAME="kirby"
+
+sudo rm -rf "/usr/local/lib/$PROJECT_NAME"
+sudo rm -f "/usr/local/bin/$PROJECT_NAME"
+
+echo "[SUCCESS] Uninstalled $PROJECT_NAME"
